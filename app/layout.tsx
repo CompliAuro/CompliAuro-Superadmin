aea52a8302406432872848c2f8a48c2509423f36
Anik <160725845+coderanik@users.noreply.github.com>
2026-07-01 02:48:44 +0530

    feat: default admin layout body background to theme background

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="h-full antialiased">
-      <body className="min-h-full flex flex-col bg-[#050505] text-white">
+      <body className="min-h-full flex flex-col bg-background text-foreground">
         {children}
       </body>
     </html>
